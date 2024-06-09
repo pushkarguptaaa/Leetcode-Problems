19d24983075898a718483edd4dc17db3c7d4183e
Pushkar Gupta <pushkardps8@gmail.com>
2024-06-09 17:26:59 +0530

    corrected bug in majority element 2

--- Arrays/MajorityElement2.java
+++ Arrays/MajorityElement2.java
@@ -39,11 +39,11 @@ class Solution1 {
         for(int i=0; i<n; i++){
             if(cnt1==0 && nums[i]!=ele2){
                 ele1= nums[i];
-                cnt1++;
+                cnt1= 1;
             }
             else if(cnt2==0 && nums[i]!=ele1){
                 ele2= nums[i];
-                cnt2++;
+                cnt2= 1;
             }
             else if(nums[i]==ele1) cnt1++;
             else if(nums[i]==ele2) cnt2++;
