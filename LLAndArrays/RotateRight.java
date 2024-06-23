b9a7f8c20f934cb044388f420c56948c87de775b
Pushkar Gupta <pushkardps8@gmail.com>
2024-06-23 16:29:07 +0530

    Rotate List

---
+++ LLAndArrays/RotateRight.java
@@ -0,0 +1,61 @@
+package LLAndArrays;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode child;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode(int val, ListNode next, ListNode child) { this.val = val; this.next = next; this.child = child; }
+}
+
+public class RotateRight {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null) return head;
+
+        for(int i=0; i<k; i++){
+            ListNode temp= head;
+            while(temp.next.next!=null){
+                temp= temp.next;
+            }
+            ListNode end= temp.next;
+                end.next= head;
+                temp.next= null;
+                head= end;
+        }
+        return head;
+    }
+}
+
+class Solution1 {
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head==null || head.next==null || k==0) return head;
+        ListNode temp= head;
+        int l= 1;
+        while(temp.next!=null){
+            l++;
+            temp= temp.next;
+        }
+
+        temp.next= head;
+        k= k%l;
+        k= l-k;
+
+        while(k>0){
+            temp= temp.next;
+            k--;
+        }
+        
+        head= temp.next;
+        temp.next= null;
+        
+        return head;
+    }
+}
