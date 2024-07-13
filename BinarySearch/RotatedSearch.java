797161e3278ce12806fbe2abcf5ff0a941b97918
Pushkar Gupta <pushkardps8@gmail.com>
2024-07-13 22:15:51 +0530

    Search in Rotated Sorted Array

---
+++ BinarySearch/RotatedSearch.java
@@ -0,0 +1,44 @@
+package BinarySearch;
+
+public class RotatedSearch {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int n= nums.length;
+
+        for(int i=0; i<n; i++){
+            if(nums[i]==target) return i;
+        }
+        return -1;
+    }
+}
+
+class Solution1 {
+    public int search(int[] nums, int target) {
+        int n= nums.length;
+        int low= 0;
+        int high= n-1;
+
+        while(low<=high){
+            int mid= (low+high)/2;
+
+            if(nums[mid]==target) return mid;
+
+            if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<=nums[mid]){
+                    high= mid-1;
+                } else low= mid+1;
+            }
+            else{
+                if(target>=nums[mid] && target<=nums[high]){
+                    low= mid+1;
+                } else high= mid-1;
+            }
+        }
+        return -1;
+    }
+}
