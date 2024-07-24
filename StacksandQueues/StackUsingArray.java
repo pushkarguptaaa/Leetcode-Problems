46a4276335180317b2722a41e2fdf1f8201b64dd
Pushkar Gupta <pushkardps8@gmail.com>
2024-07-24 13:38:32 +0530

    Implement Stack using Array

---
+++ StacksandQueues/StackUsingArray.java
@@ -0,0 +1,28 @@
+package StacksandQueues;
+
+public class StackUsingArray {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class stack {
+    int size = 10000;
+    int arr[] = new int[size];
+    int top = -1;
+    void push(int x) {
+        top++;
+        arr[top] = x;
+    }
+    int pop() {
+        int x = arr[top];
+        top--;
+        return x;
+    }
+    int top() {
+        return arr[top];
+    }
+    int size() {
+        return top + 1;
+    }
+}
