9d206839eaa826d83fdcf5ad42bfb4ae921676d3
Pushkar Gupta <pushkardps8@gmail.com>
2024-08-10 16:20:41 +0530

    Index of the First Occurrence in a String using KMP

--- Strings/IndexSubstring.java
+++ Strings/IndexSubstring.java
@@ -24,4 +24,47 @@ class Solution {
     }
 }
 
+class Solution1 {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        if(needle == null || m == 0) return 0;
+
+        int[] lps = new int[m];
+        int prev = 0;
+        int i = 1;
+
+        while(i < m){
+            if(needle.charAt(i) == needle.charAt(prev)){
+                lps[i] = prev + 1;
+                prev++;
+                i++;
+            }
+            else if(prev == 0){
+                lps[i] = 0;
+                i++;
+            }
+            else{
+                prev = lps[prev - 1];
+            }
+        }
+
+        i = 0;
+        int j = 0;
+
+        while(i < n){
+            if(haystack.charAt(i) == needle.charAt(j)){
+                i++;
+                j++;
+            }
+            else if(j == 0) i++;
+            else j = lps[j - 1];
+
+            if(j == m) return i - m;
+        }
+        return -1;
+    }
+}
+
 
