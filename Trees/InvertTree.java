d08121ccfe249606eb05fd63b402673463b795c9
Pushkar Gupta <pushkardps8@gmail.com>
2024-09-04 19:30:43 +0530

    Invert Binary Tree

---
+++ Trees/InvertTree.java
@@ -0,0 +1,22 @@
+package Trees;
+
+public class InvertTree {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+    }
+}
