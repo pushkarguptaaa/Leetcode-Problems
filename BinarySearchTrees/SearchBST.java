f2a6c7500b81f0802b82258e8f44cbcb34aa9cf7
Pushkar Gupta <pushkardps8@gmail.com>
2024-09-05 18:09:10 +0530

    Insert into a Binary Search Tree

--- BinarySearchTrees/SearchBST.java
+++ BinarySearchTrees/SearchBST.java
@@ -1,30 +1,28 @@
 package BinarySearchTrees;
 
-public class SearchBST {
-    
-  public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
- 
-
-
-class Solution {
-    public TreeNode searchBST(TreeNode root, int val) {
-        if(root == null) return null;
-
-        if(root.val == val) return root;
-        else if(root.val > val) return searchBST(root.left, val);
-
-        return searchBST(root.right, val);
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
+
+public class SearchBST {
+ 
+    class Solution {
+        public TreeNode searchBST(TreeNode root, int val) {
+            if(root == null) return null;
+
+            if(root.val == val) return root;
+            else if(root.val > val) return searchBST(root.left, val);
+
+            return searchBST(root.right, val);
+        }
+    }
 }
