e2ef119bb1fc6397439e9e36f75a9b4fda15df1b
Pushkar Gupta <pushkardps8@gmail.com>
2024-09-13 20:32:08 +0530

    Floor in BST

---
+++ Trees/Floor.java
@@ -0,0 +1,27 @@
+package Trees;
+
+public class Floor {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Solution {
+    public static int floor(TreeNode root, int x) {
+    int floor = -1;
+    
+    while(root != null){
+        if(root.val == x){
+            floor = root.val;
+            return floor;
+        }
+        
+        if(x > root.val){
+            floor = root.val;
+            root = root.right;
+        }
+        else root = root.left;
+    }
+    return floor;
+}
+}
