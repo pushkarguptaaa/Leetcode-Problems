5e6b844890928d3de1e87e0d0209522aa9e6edb7
Pushkar Gupta <pushkardps8@gmail.com>
2024-09-26 16:33:45 +0530

    Course Schedule 2

--- Graphs/CourseSchedule1.java
+++ Graphs/CourseSchedule1.java
@@ -48,3 +48,46 @@ class Solution {
         return false;
     }
 }
+
+class Solution1 {
+    public int[] findOrder(int V, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0; i < V; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < prerequisites.length; i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        int[] indegree = new int[V];
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0; i < V; i++){
+            for(int it: adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0) q.add(i);
+        }
+
+        int[] ans = new int[V];
+        int i = 0;
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ans[i++] = node;
+
+            for(int it: adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0) q.add(it);
+            }
+        }
+        if(i == V) return ans;
+
+        return new int[]{};
+    }
+}
