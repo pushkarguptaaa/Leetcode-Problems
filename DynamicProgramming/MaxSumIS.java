06951644bb8a08bcdb890874ab1b177c01b5f060
Pushkar Gupta <pushkardps8@gmail.com>
2024-10-15 16:06:20 +0530

    Max Sum Increasing Subsequence

---
+++ DynamicProgramming/MaxSumIS.java
@@ -0,0 +1,30 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class MaxSumIS {
+    public int maxSumIS(int arr[], int n)  
+	{  
+        int[][] dp = new int[n][n + 1];
+
+        for(int[] row: dp) Arrays.fill(row, -1);
+
+        return helper(arr, n, 0, -1, dp); 
+    }
+
+    int helper(int[] nums, int n, int ind, int prev, int[][] dp){
+        if(ind == n) return 0;
+ 
+        if(dp[ind][prev + 1] != -1) return dp[ind][prev + 1];
+
+        int notTake = helper(nums, n, ind + 1, prev, dp);
+
+        int take = 0;
+
+        if(prev == -1 || nums[ind] > nums[prev]){
+            take = nums[ind] + helper(nums, n, ind + 1, ind, dp);
+        }
+
+        return dp[ind][prev + 1] = Math.max(notTake, take);
+	}
+}
