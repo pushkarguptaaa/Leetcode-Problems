ae046dd4728e542e30a04993fd2e33d3e775a49d
Pushkar Gupta <pushkardps8@gmail.com>
2024-11-09 10:06:45 +0530

    Assign Cookies

---
+++ GreedyAlgorithm/AssignCookies.java
@@ -0,0 +1,21 @@
+package GreedyAlgorithm;
+
+import java.util.Arrays;
+
+public class AssignCookies {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int l = 0;
+        int r = 0;
+
+        while(l < m && r < n){
+            if(s[l] >= g[r]) r++;
+            l++;
+        }
+        return r;    
+        
+    }
+}
